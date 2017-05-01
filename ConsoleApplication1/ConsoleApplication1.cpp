82fb2e68668dd0cc429e1a4e9dfb5c0dbdd533c8
EricLIN <eric888a@hotmail.com.tw>
2017-05-01 22:42:57 +0800

    A demo ch03_01

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,10 +2,18 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
+#include "stdlib.h"
 
+using namespace std;
 
-int main()
+int main(void)
 {
+    int r = 3;
+    const float pi = 3.1416;
+    float area = pi*r*r;
+    cout << "radius is" << r << "The area of the circle is" << area << "\n";
+    system("pause");
     return 0;
 }
 
